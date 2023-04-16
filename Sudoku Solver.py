ef2f54ccb69c678b22269ca8a12c6a08d820896d
Jinyoung Rhee <jakerhee321@gmail.com>
2023-04-16 11:37:56 +0900

    Functions

--- Sudoku Solver.py
+++ Sudoku Solver.py
@@ -1,1 +1,13 @@
 from pprint import pprint
+
+def find_next_empty(puzzle):
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == -1:
+                return r, c
+    
+    return None, None
+
+def solve_sudoku(puzzle):
+
+    row, col = find_next_empty(puzzle)
